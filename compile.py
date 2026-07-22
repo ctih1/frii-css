71ac3fa20f897125e6a0b8e4af4186d9644d6955
ctih1 <onni09nevala@gmail.com>
2026-07-22 23:32:05 +0300

    Updated gitignore and script

--- compile.py
+++ compile.py
@@ -1,10 +1,15 @@
 import os
+import re
 
 lines = ""
 
 
 for file in os.listdir("css"):
     with open(f"css/{file}", encoding="utf-8") as f:
-        lines += "".join(f.readlines())
+        lines += "".join(f.readlines()) + "\n"
 
-with open("output.css")
+with open("out.min.css", "w", encoding="utf-8") as f:
+    f.write(re.sub(" +", " ", lines.replace("\n", "")))
+
+with open("out.css", "w", encoding="utf-8") as f:
+    f.write(lines)
